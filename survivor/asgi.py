25007dbd727a5615140797fece734fd223a0ccbb
jblb2424 <justin.barry97@gmail.com>
2020-07-31 13:19:01 -0400

    attempt for asgi file

--- survivor/asgi.py
+++ survivor/asgi.py
@@ -1,6 +1,9 @@
 from channels.layers import get_channel_layer
 import os
+import django
+from channels.routing import get_default_application
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "survivor.settings")
-channel_layer = get_channel_layer()
+django.setup()
+application = get_default_application()
