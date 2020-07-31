d2c72853e67af99fac936bb153cb52bde4d7b442
jblb2424 <justin.barry97@gmail.com>
2020-07-31 13:09:11 -0400

    asgi fix

--- survivor/asgi.py
+++ survivor/asgi.py
@@ -1,3 +1,5 @@
-from channels.layers import get_channel_layer
+import os
+import channels.asgi
 
-channel_layer = get_channel_layer()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "survivor.settings")
+channel_layer = channels.asgi.get_channel_layer()
