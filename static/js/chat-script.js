d69507c9def4771c1129e395d184a4c1babdc2f6
jblb2424 <justin.barry97@gmail.com>
2020-08-01 13:16:41 -0400

    domain on back button

--- static/js/chat-script.js
+++ static/js/chat-script.js
@@ -81,7 +81,7 @@ $( document ).ready(function() {
 });
 
   $('.back').click(() => {
-    location.href = "http://127.0.0.1:8000/";
+    location.href = domain + "/";
   })
   //My wonderful websocket
   var chatSocket = new WebSocket(
