3750cf54eb84baf0faab1a463a955794ea28bfa1
jblb2424 <justin.barry97@gmail.com>
2020-07-31 20:28:46 -0400

    whitenoise

--- survivor/asgi.py
+++ survivor/asgi.py
@@ -2,7 +2,8 @@ from channels.layers import get_channel_layer
 import os
 import django
 from channels.routing import get_default_application
-
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "survivor.settings")
 django.setup()
